4f0cdf01c6a578fa996f7697c598295ff8a224c0
Naywasd <sermsub2549@gmail.com>
2026-01-06 11:25:08 +0700

    Update lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= x; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
